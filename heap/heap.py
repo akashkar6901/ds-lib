cf011ed6be96a272fa18f91fc6b97f4900ec244c
akashkar6901 <akashkar6901@gmail.com>
2022-01-07 15:06:01 +0530

    updated

--- heap/heap.py
+++ heap/heap.py
@@ -1,5 +1,4 @@
-class maxheap:
-  
+class MaxHeap:
   def __init__(self):
     self.heap = [None]
     
@@ -14,37 +13,44 @@ class maxheap:
         current = parent
       else:
         break
-    return self.heap
-
+    
   def delete(self):
     current = 1
     last_index = len(self.heap)-1
+
+    #if the heap is empty
+    if(last_index == 0):
+      raise Exception("Empty heap")
         
     self.heap[current] , self.heap[last_index] =  self.heap[last_index] , self.heap[current]
     popped = self.heap.pop()
+    last_index -=1
     
     while(current < last_index):
       left_child = current*2
       right_child = left_child+1
+
+      #if the index has no child
       if(left_child > last_index):
         break
       
+      #if the index has only one child....i.e..left child
       elif(right_child > last_index):
         if(self.heap[left_child] > self.heap[current]):
           self.heap[left_child] , self.heap[current] = self.heap[current] , self.heap[left_child]
-          current = left_child
         else:
           break
-
+      
+      #if the index have both the children
       else:
         max_index = left_child
         if(self.heap[left_child] < self.heap[right_child]):
-            max_index = right_child
+          max_index = right_child
 
         if(self.heap[current] > self.heap[max_index]):
-            break
+          break
         else:
-            self.heap[current] , self.heap[max_index] = self.heap[max_index] , self.heap[current]
-            
-      return popped
-                
+          self.heap[current] , self.heap[max_index] = self.heap[max_index] , self.heap[current]
+          current = max_index
+
+    return popped                
