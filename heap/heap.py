ccd1dc9bf06bd7c7cd23b2bdd9231d9bc5da77b4
akashkar6901 <akashkar6901@gmail.com>
2022-01-17 15:06:13 +0530

    heapify added

--- heap/heap.py
+++ heap/heap.py
@@ -13,19 +13,11 @@ class MaxHeap:
         current = parent
       else:
         break
-    
-  def delete(self):
-    current = 1
-    last_index = len(self.heap)-1
 
-    #if the heap is empty
-    if(last_index == 0):
-      raise Exception("Empty heap")
-        
-    self.heap[current] , self.heap[last_index] =  self.heap[last_index] , self.heap[current]
-    popped = self.heap.pop()
-    last_index -=1
-    
+  def shiftDown(self, arr, start):
+    current = start
+    last_index = len(arr) - 1
+
     while(current < last_index):
       left_child = current*2
       right_child = left_child+1
@@ -36,21 +28,42 @@ class MaxHeap:
       
       #if the index has only one child....i.e..left child
       elif(right_child > last_index):
-        if(self.heap[left_child] > self.heap[current]):
-          self.heap[left_child] , self.heap[current] = self.heap[current] , self.heap[left_child]
+        if(arr[left_child] > arr[current]):
+          arr[left_child] , arr[current] = arr[current] , arr[left_child]
         else:
           break
       
       #if the index have both the children
       else:
         max_index = left_child
-        if(self.heap[left_child] < self.heap[right_child]):
+        if(arr[left_child] < arr[right_child]):
           max_index = right_child
 
-        if(self.heap[current] > self.heap[max_index]):
+        if(arr[current] > arr[max_index]):
           break
         else:
-          self.heap[current] , self.heap[max_index] = self.heap[max_index] , self.heap[current]
+          arr[current] , arr[max_index] = arr[max_index] , arr[current]
           current = max_index
+   
+  def delete(self):
+    current = 1
+    last_index = len(self.heap)-1
 
-    return popped                
+    #if the heap is empty
+    if(last_index == 0):
+      raise Exception("Empty heap")
+        
+    self.heap[current] , self.heap[last_index] =  self.heap[last_index] , self.heap[current]
+    popped = self.heap.pop()
+
+    self.shiftDown(self.heap, 1)
+    return popped 
+
+  def heapify(cls, arr):
+
+    for i in range((len(arr)//2) - 1, -1, -1):
+      cls.shiftDown(arr, i)
+    return arr 
+  
+  
+    
