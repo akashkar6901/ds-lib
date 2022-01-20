fd371e07914ad62528d7aff1b0ea0a383c708a3c
akashkar6901 <akashkar6901@gmail.com>
2022-01-20 11:03:17 +0530

    updated

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,14 +1,17 @@
-def BubbleSort(arr):
+def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        swap = 0
+        swap_count = 0
         for j in range(n-1):
+
             if(arr[j] < arr[j+1]):
                 arr[j] , arr[j+1] = arr[j+1] , arr[j]
-                swap += 1
-        if(swap == 0):
+                swap_count += 1
+
+        if(swap_count == 0):
+            #that means the array is already sorted so break the loop.
             break
     return arr
     
 arr = list(map(int,input("ENTER THE ARRAY:-").split(" ")))
-print(BubbleSort(arr))
+print(bubble_sort(arr))
