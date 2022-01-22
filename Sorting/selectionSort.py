8ab389e6324cce028524a65a4abfae16baa0359e
akashkar6901 <95637518+akashkar6901@users.noreply.github.com>
2022-01-22 11:21:15 +0530

    Selection sort (#7)
    * selectionSort added

---
+++ Sorting/selectionSort.py
@@ -0,0 +1,16 @@
+def get_min_index(arr, start):
+    min_index = start
+
+    for i in range(start+1 , len(arr)):
+        if(arr[i] < arr[min_index]):
+            min_index = i
+
+    return min_index
+
+def selection_sort(arr):
+    n=len(arr)
+    
+    for i in range(n-1):
+        start = i
+        min_index = get_min_index(arr, start)
+        arr[min_index], arr[i] = arr[i], arr[min_index]
