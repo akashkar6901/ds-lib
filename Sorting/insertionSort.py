811a134b85bf9898b4fef02aa5c0d834d14fa958
akashkar6901 <95637518+akashkar6901@users.noreply.github.com>
2022-01-24 11:24:47 +0530

    insertion sort added (#8)
    * insertion sort added

---
+++ Sorting/insertionSort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        j = i - 1
+        temp = arr[i]
+        
+        while(j >= 0 and arr[j] > temp):
+            arr[j+1] = arr[j]
+            j -= 1
+        
+        arr[j+1] = temp
+
